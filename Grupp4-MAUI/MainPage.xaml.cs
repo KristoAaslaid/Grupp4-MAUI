1eb597539d3da26e29c089fd137826c325bcbf13
fowortune <hendrik4131@gmail.com>
2023-04-03 14:34:05 +0300

    Accelerometer r sumn

--- Grupp4-MAUI/MainPage.xaml.cs
+++ Grupp4-MAUI/MainPage.xaml.cs
@@ -15,7 +15,7 @@ public partial class MainPage : ContentPage
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
-
+        Console.WriteLine("Button clicked");
 		if (count == 1)
 			CounterBtn.Text = $"Clicked {count} time";
 		else
@@ -23,5 +23,33 @@ public partial class MainPage : ContentPage
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
+
+    public void ToggleAccelerometer(object sender, EventArgs e)
+    {
+        if (Accelerometer.Default.IsSupported)
+        {
+            if (!Accelerometer.Default.IsMonitoring)
+            {
+                // Turn on accelerometer
+                Accelerometer.Default.ReadingChanged += Accelerometer_ReadingChanged;
+                Accelerometer.Default.Start(SensorSpeed.UI);
+                AccelToggle.Text = "Reading";
+            }
+            else
+            {
+                // Turn off accelerometer
+                Accelerometer.Default.Stop();
+                Accelerometer.Default.ReadingChanged -= Accelerometer_ReadingChanged;
+                AccelToggle.Text = "Not reading";
+            }
+        }
+    }
+
+    private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+    {
+        // Update UI Label with accelerometer state
+        AccelText.TextColor = Colors.Green;
+        AccelText.Text = $"Accel: {e.Reading}";
+    }
 }
 
